f8fcf7670853bbed6f73a42bb107ee7fcac75354
Wesley Santos <wesleysotnas64@gmail.com>
2024-08-26 08:00:45 -0300

    feat: Movimentação horizontal do Player.

--- space-invaders-clone-portfolio/Assets/Scripts/PlayerMove.cs
+++ space-invaders-clone-portfolio/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 _direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.A)) _direction += Vector2.left;
+        if(Input.GetKey(KeyCode.D)) _direction += Vector2.right;
+
+        _direction = speed * Time.deltaTime * _direction.normalized;
+        transform.Translate(_direction.x, _direction.y, 0);
     }
 }
